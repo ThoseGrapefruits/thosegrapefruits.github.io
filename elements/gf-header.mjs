6c8aba511f741c4c0a97550a11360b21ea7f5c97
Logan Moore <lmooore@gmail.com>
2020-01-19 14:21:32 -0500

    Move links next to header image

--- elements/gf-header.mjs
+++ elements/gf-header.mjs
@@ -20,21 +20,21 @@ class GFHeader extends LitElement {
         <div class="title">
           <h1>ThoseGrapefruits</h1>
           <span>It's nothing a nap can't fix.</span>
+          <div class="row">
+            <div class="links">
+              <a href="/">Home</a>
+              /
+              <a href="/about.html">About</a>
+              /
+              <a href="https://pellow.bandcamp.com/" target="_blank">Music</a>
+              /
+              <a href="https://thosegrapefruits.smugmug.com/" target="_blank">
+                Photography
+              </a>
+            </div>
+          </div>
         </div>
       </nav>
-      <div class="row">
-        <div class="links">
-          <a href="/">Home</a>
-          /
-          <a href="/about.html">About</a>
-          /
-          <a href="https://pellow.bandcamp.com/" target="_blank">Music</a>
-          /
-          <a href="https://thosegrapefruits.smugmug.com/" target="_blank">
-            Photography
-          </a>
-        </div>
-      </div>
     `;
   }
 }
