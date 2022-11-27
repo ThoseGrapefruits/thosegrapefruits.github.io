609273339a0c42956a398051012a94d41df6e1ae
Logan Moore <lmooore@gmail.com>
2022-11-27 11:34:48 -0800

    Remove `lit-element` dependency

--- elements/gf-header.mjs
+++ elements/gf-header.mjs
@@ -1,13 +1,13 @@
-import {LitElement, html, css} from 'https://unpkg.com/lit-element/lit-element.js?module';
+const LINKS = [
+  [ 'Home',  '/' ],
+  [ 'About', '/about.html' ],
+  [ 'GitHub', 'https://github.com/ThoseGrapefruits' ],
+];
 
-class GFHeader extends LitElement {
-
-  static get properties() {
-    return {};
-  }
-
-  createRenderRoot() {
-    return this;
+class GFHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.appendChild(renderNav());
   }
 
   render() {
@@ -35,3 +35,75 @@ class GFHeader extends LitElement {
 }
 
 customElements.define('gf-header', GFHeader);
+
+function renderIcon() {
+  let img = document.createElement('img');
+  img.setAttribute('src', '../assets/moon.png');
+  img.setAttribute('alt', 'Moon texture on transparent background');
+
+  let a = document.createElement('a');
+  a.setAttribute('href', '/');
+  a.appendChild(img);
+
+  return a;
+}
+
+function renderTitleAndLinks() {
+  let h1 = document.createElement('h1');
+  h1.appendChild(text('ThoseGrapefruits'));
+
+  let row = document.createElement('div');
+  row.classList.add('row');
+  row.appendChild(renderLinks());
+
+  let title = document.createElement('div');
+  title.classList.add('title');
+  title.appendChild(h1);
+  title.appendChild(row);
+
+  return title;
+}
+
+function renderLinks() {
+  let links = document.createElement('div');
+  links.classList.add('links');
+  for (let anchor of renderLinkAnchors()) {
+    links.appendChild(anchor);
+  }
+
+  return links;
+}
+
+function * renderLinkAnchors() {
+  let start = true;
+
+  for (let [ title, href ] of LINKS) {
+    if (!start) {
+      yield text(' / ');
+    }
+
+    start = false;
+
+    let link = document.createElement('a');
+    link.setAttribute('href', href);
+    link.appendChild(text(title));
+
+    if (!href.startsWith('/')) {
+      link.setAttribute('target', '_blank');
+    }
+
+    yield link;
+  }
+}
+
+function renderNav() {
+  let nav = document.createElement('nav');
+  nav.classList.add('row');
+  nav.appendChild(renderIcon());
+  nav.appendChild(renderTitleAndLinks());
+  return nav;
+}
+
+function text(t) {
+  return document.createTextNode(t);
+}
